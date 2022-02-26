3100f2aafad3a990902519d3fafa5c62993ec064
vMUSTANGv <80420955+vMUSTANGv@users.noreply.github.com>
2022-02-27 00:14:10 +0300

    ff

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-
-// You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+
+  if (matrix === undefined) { 
+    const newLocal = [];
+      return newLocal
+  } else {
+
+  let s = matrix.map((a, i) => i % 2 !== 0 ? a.sort((a, b) => b - a) : a);
+  let s1 = [].concat(...s)
+  return s1;
+  }
 }
